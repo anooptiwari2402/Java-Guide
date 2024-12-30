9bc00b624e393dbe0ba064a83a9db4ea5df93c9f
anooptiwari2402 <anoop.ece.nitap@gmail.com>
2024-12-30 22:42:25 +0530

    Variables introduction

---
+++ Day-2/src/variables/StaticVariable.java
@@ -0,0 +1,35 @@
+package variables;
+
+public class StaticVariable {
+    public static void main(String[] args){
+
+        Main main1 = new Main();
+        Main main2 = new Main();
+
+        main1.increment();
+        main2.increment();
+
+        System.out.println("Main1 withoutstatic: "+main1.getWithoutStatic());
+        System.out.println("Main1 withStatic: "+main1.getWithStaticCount());
+
+        System.out.println("Main2 withStatic: "+main2.getWithStaticCount());
+        System.out.println("Main2 withoutstatic: "+main2.getWithoutStatic());
+    }
+}
+
+class Main{
+    public static int withStaticCount = 0;
+    public int withoutStatic = 0;
+
+    public void increment(){
+        withoutStatic++;
+        withStaticCount++;
+    }
+
+    public int getWithStaticCount(){
+        return withStaticCount;
+    }
+    public int getWithoutStatic(){
+        return withoutStatic;
+    }
+}
---
+++ Day-2/src/variables/VariablesGuide.java
@@ -0,0 +1,59 @@
+package variables;
+
+public class VariablesGuide {
+    public static void main(String[] args){
+
+        /**
+         * Variables
+         *
+         * byte - 8-bit signed two's complement integer
+         * short - 16-bit signed two's complement integer
+         * int - 32-bit signed two's complement integer
+         * long - 64-bit signed two's complement integer
+         * float - 32-bit IEEE 754 floating point
+         * double - 64-bit IEEE 754 floating point
+         * char - 16-bit Unicode character
+         * boolean - true or false
+         *
+         */
+
+
+        /**
+         * All Integers without decimal
+         */
+        byte byteVariable = 127;
+        short shortVariable = 32767;
+        int intVariable = 2147483647;
+        long longVariable = 9223372036854775807L;
+
+        /**
+         * All Floats with decimal
+         */
+        float floatVariable = 3.4028235E38f;
+        double doubleVariable = 1.7976931348623157E308;
+
+        /**
+         * Character of Alphabet
+         */
+        char charVariable = 'A';
+        /**
+         * Boolean
+         */
+        boolean booleanVariable = true;
+
+
+        /**
+         * Rules
+         * 1. Variable names are case-sensitive
+         * 2. Variable names must start with a letter, $, or _
+         * 3. Variable names can contain letters, numbers, $, or _
+         * 4. Variable names cannot contain spaces
+         * 5. Variable names cannot be a reserved keyword
+         * 6. Variable names should be in camelCase
+         * 8. Variable names should not start with a number
+         */
+
+
+
+    }
+}
