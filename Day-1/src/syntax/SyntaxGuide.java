fd4dd2632e4c4840fcbea51ee5162de143def51b
anooptiwari2402 <anoop.ece.nitap@gmail.com>
2024-12-30 18:38:20 +0530

    Added Day-1 details

---
+++ Day-1/src/syntax/SyntaxGuide.java
@@ -0,0 +1,11 @@
+package syntax;
+
+/**
+ * To Guide about creating the syntax of java
+ * by creating a simple Hello World program
+ */
+public class SyntaxGuide {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
